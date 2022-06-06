1b9301f83235add7994ab76a62758f30cc225017
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2022-06-06 19:01:28 +0900

    임시

--- client.c
+++ client.c
@@ -6,14 +6,25 @@
 #include <time.h> // time()함수
 #include <Windows.h> // sleep
 
+
 void ErrorHandling(char* message);
-double RandomNum();
-void timerS();
-void randChFun(char* msg);
+double RandomNum(int randSens);  //랜덤넘버생성
+void timerS(int randSens);       //타이머 생성
+void randChFun(char* msg, int randSens); // 랜덤 값 Char형 변환
+void sendFun(SOCKET hSocket,int randSens); //서버로 송신 Function
+
+
 #define MAX_PACKET_SIZE  120
+#define MAX_SENSER_SIZE 5
+
+
+struct randRes { // 값저장을 위한 구조체 정의
+	int res;
+};
 
 int main(int argc, char* argv[])
 {
+	//기본설정 Start
 	WSADATA wsaData;
 	SOCKET hSocket;
 	SOCKADDR_IN servAdr;
@@ -44,18 +55,101 @@ int main(int argc, char* argv[])
 	else {
 		printf("Client> connection established...\n");
 	}
+	//기본설정 End
+
+	//Senser Setting Start
+
+	srand(time(NULL)); // 매번 다른 시드값 생성
+	int randSens = 0; //랜덤한 센서값 생성
+	randSens = (rand() % 5) + 1; //난수생성 // 1~5생성
+
+	int senserArr[MAX_SENSER_SIZE]; //센서배열 생성
+
+	for (int i = 0; i < randSens; i++) {
+		senserArr[i] = 0;
+	}
+
+		sendFun(hSocket,randSens);
+	
+
+	//debug Code 
+	/*
+	printf("---%d---\n", randSens);
+	for (int i = 0; i < randSens; i++) {
+		printf("%d , ", senserArr[i]);
+	}
+	//debug Code 
+	*/
+	//Senser Setting End
+
+	return 0;
+}
+
+void ErrorHandling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+double RandomNum(int randSens) {
+
+	for (int i = 0; i < randSens; i++) {
+		srand(time(NULL)); // 매번 다른 시드값 생성
+	}
+	for (int i = 0; i < randSens; i++) {
+		int randomint = 0; // 정수형 변수 선언
+		randomint = rand() % 10; // 난수 생성  // 0~9 정수값 생성
+		//printf("%d\n", randomint); // 출력 //rand()%(T-N+1)+N 
+
+		double randomReal = 0;
+		randomReal = (double)(rand() % 45) / 10;   // 0~4.5 사이 랜덤 실수값 생성
+		//printf("%f\n", randomReal); // 출력
+
+		double sumRandom = 0;
+		sumRandom = randomint + randomReal;
+		printf("%0.1f\n", sumRandom);
+
+		return sumRandom;
+	}
+}
+
+void timerS() {
+	//Timer Setting start
+	struct tm* t;
+	time_t base = time(NULL);
+	t = localtime(&base);
+	printf("%d년 %d월 %d일 %d시 %d분 %d초\n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+	//timer Setting End
+}
+
+void randChFun(char *msg ,int randSens) {
+	timerS();
+	//Random Number start
+	double rcvRand = 0;
+	rcvRand = RandomNum(randSens);
+	//printf("%f", rcvRand);
+	sprintf(msg, "%f", rcvRand);
+	//Random Number End
+}
+
+void sendFun(SOCKET hSocket,int randSens) {
 
 	char msg[MAX_PACKET_SIZE];
 	int flag;
-	int firstFlag=1;
+	int firstFlag = 1;
 	flag = 1;
+	struct randRes rR;  //rR = randRes
+
 	while (flag) {
 
 		if (firstFlag == 1) {
 			//printf("1회용");
-			randChFun(msg);
-			send(hSocket, msg, sizeof(MAX_PACKET_SIZE), 0);
-			firstFlag = 0; //최초실행후 반복문으로 실행된다.
+			for (int i = 0; i < randSens; i++) {
+				randChFun(msg,randSens);
+				send(hSocket, msg, sizeof(MAX_PACKET_SIZE), 0);
+				firstFlag = 0; //최초실행후 반복문으로 실행된다.
+			}
 			Sleep(2000);
 		}
 
@@ -63,7 +157,7 @@ int main(int argc, char* argv[])
 			int rcvflag;
 			char* fm;
 			recv(hSocket, &msg, sizeof(MAX_PACKET_SIZE), 0);
-			printf("%s", msg);
+			//printf("%s", msg);
 			fm = msg;
 			rcvflag = atof(fm);
 
@@ -78,8 +172,10 @@ int main(int argc, char* argv[])
 					flag = 0;
 				}
 				else if (selectError == 1) {
-					randChFun(msg);
-					send(hSocket, msg, sizeof(MAX_PACKET_SIZE), 0);
+					for (int i = 0; i < randSens; i++) {
+						randChFun(msg, randSens);
+						send(hSocket, msg, sizeof(MAX_PACKET_SIZE), 0);
+					}
 					Sleep(2000);
 				}
 				else {
@@ -87,8 +183,10 @@ int main(int argc, char* argv[])
 				}
 			}
 			else {
-				randChFun(msg);
-				send(hSocket, msg, sizeof(MAX_PACKET_SIZE), 0);
+				for (int i = 0; i < randSens; i++) {
+					randChFun(msg, randSens));
+					send(hSocket, msg, sizeof(MAX_PACKET_SIZE), 0);
+				}
 				Sleep(2000);
 			}
 		}
@@ -98,50 +196,5 @@ int main(int argc, char* argv[])
 	closesocket(hSocket);
 	printf("Client> close socket...\n");
 	WSACleanup();
-	
-	return 0;
-}
 
-void ErrorHandling(char* message)
-{
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
-}
-
-double RandomNum() {
-
-	srand(time(NULL)); // 매번 다른 시드값 생성
-	int randomint = 0; // 정수형 변수 선언
-	randomint = rand() % 10; // 난수 생성  // 0~9 정수값 생성
-	//printf("%d\n", randomint); // 출력 //rand()%(T-N+1)+N 
-
-	double randomReal = 0;
-	randomReal = (double)(rand() % 45) / 10;   // 0~4.5 사이 랜덤 실수값 생성
-	//printf("%f\n", randomReal); // 출력
-
-	double sumRandom = 0;
-	sumRandom = randomint + randomReal;
-	printf("%0.1f\n", sumRandom);
-
-	return sumRandom;
-}
-
-void timerS() {
-	//Timer Setting start
-	struct tm* t;
-	time_t base = time(NULL);
-	t = localtime(&base);
-	printf("%d년 %d월 %d일 %d시 %d분 %d초\n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
-	//timer Setting End
-}
-
-void randChFun(char *msg) {
-	timerS();
-	//Random Number start
-	double rcvRand = 0;
-	rcvRand = RandomNum();
-	printf("%f", rcvRand);
-	sprintf(msg, "%f", rcvRand);
-	//Random Number End
 }
